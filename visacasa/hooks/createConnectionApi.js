cd2b69ae0ec0507f30189176987561f721c7f87a
Mauro Patricio <mpatricio@cedsif.gov.mz>
2025-05-23 14:04:55 +0200

    Actualizacao

--- visacasa/hooks/createConnectionApi.js
+++ visacasa/hooks/createConnectionApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
- //const instance = axios.create({baseURL: 'http://deliveryshop.herokuapp.com/api'});
- const instance = axios.create({baseURL: 'http://localhost:5000/api'})
+ const instance = axios.create({baseURL: 'https://deliveryshop.herokuapp.com/api'});
+//  const instance = axios.create({baseURL: 'http://localhost:5000/api'})
 
 
  export  const registerNotification = async data => {
